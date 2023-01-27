6d1da8f4c64b62c235122e63829cfe3090cefafe
Godfrey <kellemanatser@gmail.com>
2023-01-27 12:26:42 +0100

    Complete package

---
+++ task-managerApp/controllers/tasks.js
@@ -0,0 +1,57 @@
+const Task = require('../models/tasks_model')
+const asyncWrapper = require('../middleware/async')
+const {createCustomError} = require('../errors/custom-error')
+
+// creating task
+const createTask = asyncWrapper(async(req, res)=>{
+	const task = await Task.create(req.body)
+	res.status(201).json({task})
+})
+
+// get all tasks
+const getAllTasks = asyncWrapper(async(req, res)=>{
+	const tasks = await Task.find({})
+	res.status(200).json({tasks})
+})
+
+// find a task
+const getTask = asyncWrapper(async(req, res, next)=>{
+	const {id: taskID} = req.params
+	const task = await Task.findOne({_id: taskID})
+	if(!task){
+		return next(createCustomError(`no task with the id: ${taskID}`, 400))
+	} 
+	res.status(201).json({task})
+})
+
+// deleting a task
+const deleteTask = asyncWrapper(async(req, res, next)=>{
+	const {id: taskID} = req.params 
+	const task = await Task.findOneAndDelete({_id: taskID})
+	if(!task){
+		return next(createCustomError(`No task with id: ${taskID}`, 400))
+	}
+	res.status(200).json({task})
+})
+
+// updating a task
+const editTask = asyncWrapper(async(req, res, next)=>{
+	const {id: taskID} = req.params 
+	const task  = await Task.findOneAndUpdate({_id: taskID}, req.body, {
+		new: true,
+		runValidators: true
+	})
+	if(!task){
+		return next(createCustomError(`No task with the id: ${taskID}`, 400))
+	}
+	res.status(201).json({task})
+})
+
+module.exports = {
+	createTask,
+	getAllTasks,
+	getTask,
+	deleteTask,
+	editTask
+
+}
